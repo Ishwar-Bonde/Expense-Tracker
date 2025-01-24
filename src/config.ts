d725166025c8ee29fe64d4099fc6210c7be6c14c
Ishwar Bonde <ishwarbonde40@gmail.com>
2025-01-24 21:54:52 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = 'http://192.168.0.100:5000';
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://expense-tracker-frontend-4316.onrender.com';
