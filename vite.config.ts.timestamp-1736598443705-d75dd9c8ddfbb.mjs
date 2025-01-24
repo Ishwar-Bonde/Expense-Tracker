7b12c1c30739485d5c3579189f30fdcee425c649
Ishwar Bonde <ishwarbonde40@gmail.com>
2025-01-24 08:18:06 +0530

    Update vite.config.ts.timestamp-1736598443705-d75dd9c8ddfbb.mjs

--- vite.config.ts.timestamp-1736598443705-d75dd9c8ddfbb.mjs
+++ vite.config.ts.timestamp-1736598443705-d75dd9c8ddfbb.mjs
@@ -8,7 +8,7 @@ var vite_config_default = defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "https://expense-tracker-backend-v9yu.onrender.com",
+        target: "https://expense-tracker-backend-d14o.onrender.com",
         changeOrigin: true,
         secure: false
       }
