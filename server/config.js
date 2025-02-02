42d9960d4c30763503ef4ea18382eea54af536a6
Ishwar Bonde <ishwarbonde40@gmail.com>
2025-02-02 12:13:48 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,2 +1,5 @@
 export const API_URL_FRONTEND = 'https://expense-tracker-frontend-4316.onrender.com';
-
+export const ALLOWED_ORIGINS = [
+    'https://expense-tracker-frontend-4316.onrender.com',
+    'http://localhost:5173', // For local development
+];
