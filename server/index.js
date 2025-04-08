92afafa4f332976f781e89c9938a324e8a05635b
Ishwar Bonde <ishwarbonde40@gmail.com>
2025-04-08 11:28:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,13 @@ const app = express();
 
 // Configure CORS with specific options
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: (origin, callback) => {
+    if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
