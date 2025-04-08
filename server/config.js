3e1f293cc4d5f043806b081ba655e670c2383038
Ishwar Bond <ishwarbonde40@gmail.com>
2025-04-08 07:48:48 +0530

    Update config.js to include production URL in ALLOWED_ORIGINS

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 export const API_URL_FRONTEND = 'https://expense-tracker-frontend-4316.onrender.com';
 export const ALLOWED_ORIGINS = [
-    // 'https://expense-tracker-frontend-4316.onrender.com',
+    'https://expense-tracker-frontend-4316.onrender.com',
     'http://localhost:5173', // For local development
 ];
